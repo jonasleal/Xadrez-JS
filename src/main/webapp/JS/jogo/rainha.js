2951ecf421d72b191088dc5adf17e11f14b2cda2
Samuel Soares <samuelsoares179@gmail.com>
2017-06-07 18:17:43 -0300

    adicionando metodos a rainha

--- src/main/webapp/JS/jogo/rainha.js
+++ src/main/webapp/JS/jogo/rainha.js
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 
-var Rainha = function (_cor,_linha,_coluna){
+var Rainha = function (_cor, _linha, _coluna) {
     var id = "Rainha-";
     var imagem = document.createElement("img");
+
+    if (_cor === 0) {
+        id = id + "Branco-" + _linha + "" + _coluna;
+        imagem.src = "JS/jogo/pecas/rainha-branco.png";
+    } else {
+        id = id + "Preto-" + _linha + "" + _coluna;
+        imagem.src = "JS/jogo/pecas/rainha-preto.png";
+    }
     
     
+    imagem.id = id;
+    imagem.width = "50";
+    imagem.height = "50";
+    imagem.class = "drag";
+
+
+    Rainha.movimento = function(_linha,_coluna,_cor){
+        
+    };
     
+    Rainha.inicioMovimento = function(event,ui){
+        var linha = Number (ui.helper.context.parentNode.id.split(""[0]));
+        var coluna = Number(ui.helper.context.parentNode.id.split("")[1]);
+        var corPeca = ui.helper.context.id.split("-")[1];
+        
+        var movimentos = Rainha.movimento(linha,coluna,corPeca);
+        
+        for (var i = 0; i < movimentos.length; i++){
+            $("#" + movimentos[i]).droppable({scope: id, drop: Rainha.jogada});
+        }
+    };
     
+    Rainha.jogada = function (event,ui){
+        var idCasa = event.target.id;
+        var idPeca = ui.helper.context.id;
+        var casa = $("#" + idCasa)[0];
+        var removido;
+    };
+
+
+
+
 };
