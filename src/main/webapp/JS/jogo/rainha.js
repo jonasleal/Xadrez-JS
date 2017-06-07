9e26062844d29a515f79b56525a78249077d95a0
Samuel Soares <samuelsoares179@gmail.com>
2017-06-06 21:01:36 -0300

    adicionando rainha.js ao jogo

---
+++ src/main/webapp/JS/jogo/rainha.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var Rainha = function (_cor,_linha,_coluna){
+    var id = "Rainha-";
+    var imagem = document.createElement("img");
+    
+    
+    
+    
+};
