c4dfbcf4cc68cc09e505724b85e5c93e45455321
Samuel Soares <samuelsoares179@gmail.com>
2017-06-12 15:43:55 -0300

    Adicionando modificações de rainha ao branch master

--- src/main/webapp/JS/jogo/rainha.js
+++ src/main/webapp/JS/jogo/rainha.js
@@ -24,38 +24,127 @@ var Rainha = function (_cor, _linha, _coluna) {
 
 
     Rainha.movimento = function (_linha, _coluna, _cor) {
-        
         var posicoes = new Array();
-        for (var i = -1, pos = 0; i < 2; i++) {
-            for (var j = -1; j < 2; j++) {
-                if (i !== 0 || j !== 0) {
-                    var linha = (Number(_linha) + i);
-                    var coluna = (Number(_coluna) + j);
-                    if (linha > -1 && coluna > -1 && linha < 8 && coluna < 8) {
-                        var indice = linha + "" + coluna;
-                        var casa = $("#" + indice)[0];
-                        if (casa.childElementCount < 1) {
-                            posicoes[pos] = linha + "" + coluna;
-                            pos++;
-                        } else {
-                            var corOutraPeca = casa.firstElementChild.id.split("-")[1];
-                            if (corOutraPeca !== _cor) {
-                                posicoes[pos] = linha + "" + coluna;
-                                pos++;
 
-                            }
-                        }
+        var j = 1;
+        var pos = 0;
+        for (var i = 1; i < 8; i++) {
+
+            var linha = (Number(_linha) + i);
+            var coluna = (Number(_coluna) + j);
+
+            if (linha > 0 && coluna > 0 && linha < 8 && coluna < 8) {
+                var indice = linha + "" + coluna;
+                var casa = $("#" + indice)[0];
+                if (casa.childElementCount < 1) {
+                    posicoes[pos] = linha + "" + coluna;
+                    pos++;
+                } else {
+
+                    var corOutraPeca = casa.firstElementChild.id.split("-")[1];
+
+                    if (corOutraPeca === _cor) {
+                        break;
+                    } else if (corOutraPeca !== _cor) {
+                        posicoes[pos] = linha + "" + coluna;
+                        pos++;
+                        break;
                     }
-
                 }
             }
+
+            j++;
+        }
+        ;
+        j = -1;
+        for (var i = 1; i < 8; i++) {
+
+            var linha = (Number(_linha) + i);
+            var coluna = (Number(_coluna) + j);
+
+            if (linha > 0 && coluna > -1 && linha < 8 && coluna < 8) {
+                var indice = linha + "" + coluna;
+                var casa = $("#" + indice)[0];
+                if (casa.childElementCount < 1) {
+                    posicoes[pos] = linha + "" + coluna;
+                    pos++;
+                } else {
+
+                    var corOutraPeca = casa.firstElementChild.id.split("-")[1];
+
+                    if (corOutraPeca === _cor) {
+                        break;
+
+                    } else if (corOutraPeca !== _cor) {
+                        posicoes[pos] = linha + "" + coluna;
+                        pos++;
+                        break;
+                    }
+                }
+            }
+
+            j--;
+        }
+        var i = -1;
+        for (var j = 1; j < 8; j++) {
+
+            var linha = (Number(_linha) + i);
+            var coluna = (Number(_coluna) + j);
+
+            if (linha > -1 && coluna > 0 && linha < 8 && coluna < 8) {
+                var indice = linha + "" + coluna;
+                var casa = $("#" + indice)[0];
+                if (casa.childElementCount < 1) {
+                    posicoes[pos] = linha + "" + coluna;
+                    pos++;
+                } else {
+
+                    var corOutraPeca = casa.firstElementChild.id.split("-")[1];
+                    if (corOutraPeca === _cor) {
+                        break;
+
+                    } else if (corOutraPeca !== _cor) {
+                        posicoes[pos] = linha + "" + coluna;
+                        pos++;
+                        break;
+                    }
+                }
+            }
+
+            i--;
+        }
+        var i = -1;
+        for (var j = -1; j > -8; j--) {
+
+            var linha = (Number(_linha) + i);
+            var coluna = (Number(_coluna) + j);
+
+            if (linha > -1 && coluna > -1 && linha < 8 && coluna < 8) {
+                var indice = linha + "" + coluna;
+                var casa = $("#" + indice)[0];
+                if (casa.childElementCount < 1) {
+                    posicoes[pos] = linha + "" + coluna;
+                    pos++;
+                } else {
+
+                    var corOutraPeca = casa.firstElementChild.id.split("-")[1];
+                    if (corOutraPeca === _cor) {
+                        break;
+                    } else if (corOutraPeca !== _cor) {
+                        posicoes[pos] = linha + "" + coluna;
+                        pos++;
+                        break;
+                    }
+                }
+            }
+
+            i--;
         }
         return posicoes;
-
     };
 
     Rainha.inicioMovimento = function (event, ui) {
-        var linha = Number(ui.helper.context.parentNode.id.split(""[0]));
+        var linha = Number(ui.helper.context.parentNode.id.split("")[0]);
         var coluna = Number(ui.helper.context.parentNode.id.split("")[1]);
         var corPeca = ui.helper.context.id.split("-")[1];
 
@@ -72,9 +161,9 @@ var Rainha = function (_cor, _linha, _coluna) {
         var casa = $("#" + idCasa)[0];
         var removido;
 
-
         if (casa.childElementCount > 0) {
             removido = casa.removeChild(casa.firstChild);
+
         }
 
         $("#" + idPeca).css({top: "8%", left: "10%"});
